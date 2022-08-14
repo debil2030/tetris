8638a92867ea91021a3160b178e7277ccb8cb6e9
debil2030 <111249002+debil2030@users.noreply.github.com>
2022-08-15 01:02:21 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 setInterval(() => {
   score += 1;
   document.getElementById("score").innerHTML = score;
-  document.getElementById("square").style.top = score *10 + "px";
-  console.log(1)
+  document.getElementById("square1").style.top = score *10 + "px";
+  document.getElementById("square2").style.top = score *10 + "px";
+  console.log(score)
 }, 500)
